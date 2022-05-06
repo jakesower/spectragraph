f56b1393c19244f0a5ab36dcb22b3b13c6563b9e
Jake Sower <jake@jakesower.com>
2022-05-05 22:57:33 -0400

    minor package updates

--- packages/memory-store/src/memory-store/operations/compile-constraints.mjs
+++ packages/memory-store/src/memory-store/operations/compile-constraints.mjs
@@ -6,17 +6,6 @@ export function compileConstraints(query) {
 
   // 3 possibilities: expression, field w/constant field w/expression
   const { constraints } = query;
-
-  // field filters are equivalent to prop lookups, but must be handled with
-  // care, seeing as they may be nested in such things as `$and` and not merely
-  // at the top level, may span resources on the schema, etc. thus, some kind
-  // context may be inescapable, but this is as yet unknown
-
-  // IGNORE BELOW
-  // field fielters and expression filters are quite different
-  // - field filters pass along the field to expressions
-  // - this needs to be baked into functions produced below
-  // IGNORE ABOVE
   const constraintFilters = Object.entries(constraints).map(
     ([constraintKey, constraintVal]) => (
       constraintDefinitions[constraintKey]
