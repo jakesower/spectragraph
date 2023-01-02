9825408cbee353804f2cce1c315e5c0fcaca1667
Jake Sower <jake@jakesower.com>
2023-01-01 22:39:31 -0800

    some css

--- packages/core/src/ui/navigation.tsx
+++ packages/core/src/ui/navigation.tsx
@@ -4,16 +4,26 @@ import { Component } from "solid-js";
 export const Navigation: Component = () => {
 	return (
 		<nav class="navigation">
-			<h1>Data Prism</h1>
-			<A href="/import" class="import">
-				Import
-			</A>
-			<A href="/remix" class="remix">
-				Remix
-			</A>
-			<A href="/chart" class="chart">
-				Chart
-			</A>
+			<section class="title">Data Prism</section>
+			<section class="controls left">
+				<button class="control sources" type="button">
+					Sources
+				</button>
+			</section>
+			<section class="controls right">
+				<button class="control sources" type="button">
+					Columns
+				</button>
+				<button class="control sources" type="button">
+					Filters
+				</button>
+				<button class="control sources" type="button">
+					Sorting
+				</button>
+				<button class="control sources" type="button">
+					Pages
+				</button>
+			</section>
 		</nav>
 	);
 };
