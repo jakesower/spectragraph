4dd8b7b1726ca8679820e271b9753140ec02b144
Jake Sower <jake@jakesower.com>
2023-02-17 15:19:30 -0800

    get tests pass

--- packages/memory-store/src/memory-store.ts
+++ packages/memory-store/src/memory-store.ts
@@ -1,14 +1,14 @@
 import { difference, mapValues, merge, pick } from "lodash-es";
 import { Query, Subquery, ensureValidQuery } from "./query.js";
-import { LooseSchema, Schema } from "./schema.js";
-import { evaluateId } from "./expressions.js";
+import { LooseSchema, Schema, compileSchema } from "./schema.js";
+import { evaluateId } from "./query.js";
 
 type MemoryStoreOptions = {
 	initialData: any;
 };
 
 export function MemoryStore(looseSchema: LooseSchema, options?: MemoryStoreOptions) {
-	const schema = looseSchema as Schema;
+	const schema = compileSchema(looseSchema);
 	const data = mapValues(schema.resources, () => ({}));
 
 	const seed = (seedData) => {
@@ -20,20 +20,41 @@ export function MemoryStore(looseSchema: LooseSchema, options?: MemoryStoreOptio
 
 		// create a function that takes in the various parts of a query and returns a
 		// function that takes query arguments and returns the result
-		const compileSubquery = (resType: string, subquery: Subquery) => {
-			return (args) => {
+		return (args) => {
+			const compileSubquery = (resType: string, subquery: Subquery) => {
+				const resDef = schema.resources[resType];
+
+				const getProperties = (res) => {
+					const props = subquery.properties ?? { [resDef.idField]: {} as Subquery };
+					return mapValues(props, (propQuery, propName) => {
+						if (propName in resDef.relationships) {
+							const relDef = resDef.relationships[propName];
+
+							return relDef.cardinality === "one"
+								? res[propName]
+									? compileSubquery(relDef.resource, { ...propQuery, id: res[propName] })
+									: null
+								: res[propName].map((id) =>
+									compileSubquery(relDef.resource, { ...propQuery, id }),
+								  );
+						}
+
+						return res[propName];
+					});
+				};
+
 				if ("id" in subquery) {
 					const id = evaluateId(subquery.id, args);
 					const res = data[resType][id];
 
-					return res ? pick(res, Object.keys(subquery.properties)) : null;
+					return res ? getProperties(res) : null;
 				}
 
-				return resType;
+				return Object.values(data[resType]).map(getProperties);
 			};
-		};
 
-		return compileSubquery(rootQuery.type, rootQuery);
+			return compileSubquery(rootQuery.type, rootQuery);
+		};
 	};
 
 	const get = (rootQuery: Query, args = {}) => {
--- packages/memory-store/test/get.test.ts
+++ packages/memory-store/test/get.test.ts
@@ -17,7 +17,7 @@ beforeEach<LocalTestContext>((context) => {
 
 // Actual Tests
 
-it.only<LocalTestContext>("fetches a single resource", async (context) => {
+it<LocalTestContext>("fetches a single resource", async (context) => {
 	const result = await context.store.get({
 		type: "bears",
 		id: "1",
@@ -29,186 +29,189 @@ it.only<LocalTestContext>("fetches a single resource", async (context) => {
 	expect(result).toEqual({ name: "Tenderheart Bear" });
 });
 
-// it<LocalTestContext>("fetches multiple resources", async (context) => {
-// 	const result = await context.store.get({ type: "bears" });
-// 	const expected = ["1", "2", "3", "5"].map((id) => ({ id }));
+it<LocalTestContext>("fetches a single resource with its id", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		id: "1",
+		properties: {
+			id: {},
+			name: {},
+		},
+	});
 
-// 	expect(result).toEqual(expected);
-// });
+	expect(result).toEqual({ id: "1", name: "Tenderheart Bear" });
+});
 
-// it<LocalTestContext>("fetches null for a nonexistent resource", async (context) => {
-// 	const result = await context.store.get({ type: "bears", id: "6" });
+it<LocalTestContext>("fetches a single resource with its id implicitly", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		id: "1",
+	});
 
-// 	expect(result).toEqual(null);
-// });
+	expect(result).toEqual({ id: "1" });
+});
 
-// it<LocalTestContext>("fetches a single resource specifying no subqueries desired", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		id: "1",
-// 		rels: {},
-// 	});
+it<LocalTestContext>("fetches a single resource without its id", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		id: "1",
+		properties: {
+			name: {},
+		},
+	});
 
-// 	expect(result).toEqual({ id: "1" });
-// });
+	expect(result).toEqual({ name: "Tenderheart Bear" });
+});
 
-// it<LocalTestContext>("fetches a single resource with a many-to-one relationship", async (context) => {
-// 	const q = {
-// 		type: "bears",
-// 		id: "1",
-// 		rels: { home: {} },
-// 	};
+it<LocalTestContext>("fetches multiple resources", async (context) => {
+	const result = await context.store.get({ type: "bears" });
+	const expected = ["1", "2", "3", "5"].map((id) => ({ id }));
 
-// 	const result = await context.store.get(q);
+	expect(result).toEqual(expected);
+});
 
-// 	expect(result).toEqual({
-// 		id: "1",
-// 		home: { id: "1" },
-// 	});
-// });
+it<LocalTestContext>("fetches a property from multiple resources", async (context) => {
+	const result = await context.store.get({ type: "bears", properties: { name: {} } });
+	const expected = [
+		"Tenderheart Bear",
+		"Cheer Bear",
+		"Wish Bear",
+		"Smart Heart Bear",
+	].map((name) => ({ name }));
 
-// it<LocalTestContext>("a single resource with a one-to-many relationship", async (context) => {
-// 	const q = {
-// 		type: "homes",
-// 		id: "1",
-// 		rels: { bears: {} },
-// 	};
+	expect(result).toEqual(expected);
+});
 
-// 	const result = await context.store.get(q);
+it<LocalTestContext>("fetches null for a nonexistent resource", async (context) => {
+	const result = await context.store.get({ type: "bears", id: "6" });
 
-// 	expect(result).toEqual({
-// 		id: "1",
-// 		bears: [{ id: "1" }, { id: "2" }, { id: "3" }],
-// 	});
-// });
+	expect(result).toEqual(null);
+});
 
-// it<LocalTestContext>("fetches a single resource with a subset of props", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		id: "1",
-// 		props: ["name", "fur_color"],
-// 	});
+it<LocalTestContext>("fetches a single resource with a many-to-one relationship", async (context) => {
+	const q = {
+		type: "bears",
+		id: "1",
+		properties: {
+			home: {},
+		},
+	};
 
-// 	expect(result).toEqual({ id: "1", name: "Tenderheart Bear", fur_color: "tan" });
-// });
+	const result = await context.store.get(q);
 
-// it<LocalTestContext>("fetches a single resource with a subset of props on a relationship", async (context) => {
-// 	const q = {
-// 		type: "bears",
-// 		id: "1",
-// 		rels: { home: { props: ["caring_meter"] } },
-// 	};
+	expect(result).toEqual({
+		home: { id: "1" },
+	});
+});
 
-// 	const result = await context.store.get(q);
+it<LocalTestContext>("a single resource with a one-to-many relationship", async (context) => {
+	const q = {
+		type: "homes",
+		id: "1",
+		properties: { residents: {} },
+	};
 
-// 	t.like(result, { id: "1", home: { id: "1", caring_meter: 1 } });
-// });
+	const result = await context.store.get(q);
 
-// it<LocalTestContext>("uses explicitly set id fields", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "powers",
-// 		id: "careBearStare",
-// 	});
+	expect(result).toEqual({
+		residents: [{ id: "1" }, { id: "2" }, { id: "3" }],
+	});
+});
 
-// 	expect(result).toEqual({ powerId: "careBearStare" });
-// });
+it<LocalTestContext>("fetches a single resource with a subset of props", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		id: "1",
+		properties: { id: {}, name: {}, furColor: {} },
+	});
 
-// it<LocalTestContext>("always returns explicitly set id fields", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "powers",
-// 	});
+	expect(result).toEqual({ id: "1", name: "Tenderheart Bear", furColor: "tan" });
+});
 
-// 	expect(result).toEqual([{ powerId: "careBearStare" }, { powerId: "makeWish" }]);
-// });
+it<LocalTestContext>("fetches a single resource with a subset of props on a relationship", async (context) => {
+	const q = {
+		type: "bears",
+		id: "1",
+		properties: { home: { properties: { caringMeter: {} } } },
+	};
 
-// it<LocalTestContext>("fetches a single resource with many-to-many relationship", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		id: "1",
-// 		rels: { powers: {} },
-// 	});
+	const result = await context.store.get(q);
 
-// 	expect(result).toEqual({ id: "1", powers: [{ powerId: "careBearStare" }] });
-// });
+	expect(result).toEqual({ home: { caringMeter: 1 } });
+});
 
-// it<LocalTestContext>("fetches multiple sub queries of various types", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		id: "1",
-// 		rels: {
-// 			home: {
-// 				rels: {
-// 					bears: {},
-// 				},
-// 			},
-// 			powers: {},
-// 		},
-// 	});
+it<LocalTestContext>("uses explicitly set id fields", async (context) => {
+	const result = await context.store.get({
+		type: "powers",
+		id: "careBearStare",
+	});
 
-// 	expect(result).toEqual({
-// 		id: "1",
-// 		home: { id: "1", bears: [{ id: "1" }, { id: "2" }, { id: "3" }] },
-// 		powers: [{ powerId: "careBearStare" }],
-// 	});
-// });
+	expect(result).toEqual({ powerId: "careBearStare" });
+});
 
-// it<LocalTestContext>("handles sub queries between the same type", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		rels: {
-// 			best_friend: {},
-// 		},
-// 	});
+it<LocalTestContext>("always returns explicitly set id fields", async (context) => {
+	const result = await context.store.get({
+		type: "powers",
+	});
 
-// 	expect(result).toEqual([
-// 		{ id: "1", best_friend: null },
-// 		{ id: "2", best_friend: { id: "3" } },
-// 		{ id: "3", best_friend: { id: "2" } },
-// 		{ id: "5", best_friend: null },
-// 	]);
-// });
+	expect(result).toEqual([{ powerId: "careBearStare" }, { powerId: "makeWish" }]);
+});
 
-// it<LocalTestContext>("fetches all props", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		id: "1",
-// 		allProps: true,
-// 	});
+it<LocalTestContext>("fetches a single resource with many-to-many relationship", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		id: "1",
+		properties: { powers: {} },
+	});
 
-// 	expect(result).toEqual(omit(careBearData.bears[1], ["home", "best_friend", "powers"]));
-// });
+	expect(result).toEqual({ powers: [{ powerId: "careBearStare" }] });
+});
 
-// it<LocalTestContext>("fetches all props except", async (context) => {
-// 	const result = await context.store.get({
-// 		type: "bears",
-// 		id: "1",
-// 		allProps: true,
-// 		excludedProps: ["belly_badge"],
-// 	});
+it<LocalTestContext>("fetches multiple sub queries of various types", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		id: "1",
+		properties: {
+			home: {
+				properties: {
+					residents: {},
+				},
+			},
+			powers: {},
+		},
+	});
 
-// 	t.deepEqual(
-// 		result,
-// 		omit(careBearData.bears[1], ["belly_badge", "home", "best_friend", "powers"]),
-// 	);
-// });
+	expect(result).toEqual({
+		home: { residents: [{ id: "1" }, { id: "2" }, { id: "3" }] },
+		powers: [{ powerId: "careBearStare" }],
+	});
+});
 
-// it<LocalTestContext>("fails validation for invalid types", async (context) => {
-// 	const err = await t.throwsAsync(async () => {
-// 		await context.store.get({ type: "bearz", id: "1" });
-// 	});
+it<LocalTestContext>("handles sub queries between the same type", async (context) => {
+	const result = await context.store.get({
+		type: "bears",
+		properties: {
+			id: {},
+			bestFriend: {},
+		},
+	});
 
-// 	t.deepEqual(err.message, ERRORS.INVALID_GET_QUERY_SYNTAX);
-// });
+	expect(result).toEqual([
+		{ id: "1", bestFriend: null },
+		{ id: "2", bestFriend: { id: "3" } },
+		{ id: "3", bestFriend: { id: "2" } },
+		{ id: "5", bestFriend: null },
+	]);
+});
 
-// it<LocalTestContext>("fails validation for invalid top level props", async (context) => {
-// 	const err = await t.throwsAsync(async () => {
-// 		await context.store.get({ type: "bears", id: "1", koopa: "troopa" });
-// 	});
+it<LocalTestContext>("fails validation for invalid types", async (context) => {
+	expect(async () => {
+		await context.store.get({ type: "bearz", id: "1" });
+	}).rejects.toThrowError();
+});
 
-// 	t.deepEqual(err.message, ERRORS.INVALID_GET_QUERY_SYNTAX);
-// });
-
-// it<LocalTestContext>("validates without an id", async (context) => {
-// 	const result = await context.store.get({ type: "bears" });
-// 	t.assert(Array.isArray(result));
-// });
+it<LocalTestContext>("fails validation for invalid top level props", async (context) => {
+	expect(async () => {
+		await context.store.get({ type: "bears", id: "1", properties: { koopa: {} } });
+	}).rejects.toThrowError();
+});
