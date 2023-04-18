b2f6f15fe6388528fd2e4fbefa4a55a51bd98bd9
Jake Sower <jake@jakesower.com>
2023-04-18 09:23:48 -0700

    simplify expression footprint

--- packages/expression/test/distribute.test.ts
+++ packages/expression/test/distribute.test.ts
@@ -1,18 +1,20 @@
 import { describe, expect, it } from "vitest";
 import { comparativeDefinitions } from "../src/definitions/comparative.js";
-import { expressionContext } from "../src/expression.js";
+import { distribute } from "../src/distribute.js";
+import { logicalDefinitions } from "../src/definitions/logical.js";
 
-const { distribute } = expressionContext(comparativeDefinitions);
+const dist = (expr) =>
+	distribute(expr, { ...comparativeDefinitions, ...logicalDefinitions });
 
 describe("expressions in the property form transforming to the canonical form", () => {
 	it("works for one equality expression", () => {
-		expect(distribute({ name: "ximena" })).toEqual({
+		expect(dist({ name: "ximena" })).toEqual({
 			$pipe: [{ $prop: "name" }, { $eq: "ximena" }],
 		});
 	});
 
 	it("works for two equality expressions", () => {
-		expect(distribute({ name: "ximena", age: 4 })).toEqual({
+		expect(dist({ name: "ximena", age: 4 })).toEqual({
 			$and: [
 				{ $pipe: [{ $prop: "name" }, { $eq: "ximena" }] },
 				{ $pipe: [{ $prop: "age" }, { $eq: 4 }] },
@@ -21,18 +23,13 @@ describe("expressions in the property form transforming to the canonical form", 
 	});
 
 	it("allows for arrays", () => {
-		expect(distribute({ hobbies: ["dancing", "coloring"] })).toEqual({
+		expect(dist({ hobbies: ["dancing", "coloring"] })).toEqual({
 			$pipe: [{ $prop: "hobbies" }, { $eq: ["dancing", "coloring"] }],
 		});
 	});
 
-	it("transforms whole expressions untouched", () => {
-		const expr = { $pipe: [{ $prop: "age" }, { $eq: 4 }] };
-		expect(distribute(expr)).toEqual(expr);
-	});
-
 	it("distributes over $and", () => {
-		expect(distribute({ $and: [{ name: "ximena" }, { age: { $gt: 4 } }] })).toEqual({
+		expect(dist({ $and: [{ name: "ximena" }, { age: { $gt: 4 } }] })).toEqual({
 			$and: [
 				{ $pipe: [{ $prop: "name" }, { $eq: "ximena" }] },
 				{ $pipe: [{ $prop: "age" }, { $gt: 4 }] },
