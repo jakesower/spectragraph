be7ac0b83b67688cafa56287a3a2cd482cfd102a
Jake Sower <jsower.redzone.co>
2024-07-19 08:55:42 -0700

    combine jsonapi packages; get unit tests working

---
+++ packages/jsonapi/src/parse-response.ts
@@ -0,0 +1,27 @@
+import { Schema, queryGraph } from "data-prism";
+import { mapValues } from "lodash-es";
+
+export function parseResponse(schema: Schema, query, response) {
+	const graph = mapValues(schema.resources, () => ({}));
+	const dataArray = Array.isArray(response.data)
+		? response.data
+		: [response.data];
+
+	const extractResource = (datum) => {
+		const resSchema = schema.resources[datum.type];
+
+		graph[datum.type][datum.id] = {
+			...datum,
+			attributes: {
+				[resSchema.idField ?? "id"]: datum.id,
+				...datum.attributes,
+			},
+			relationships: mapValues(datum.relationships ?? {}, (r) => r.data),
+		};
+	};
+
+	dataArray.forEach(extractResource);
+	(response.included ?? []).forEach(extractResource);
+
+	return queryGraph(graph, query);
+}
