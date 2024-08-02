200cf979831c7b07d023bf041511f5530f6563de
Jake Sower <jsower.redzone.co>
2024-08-02 09:25:31 -0700

    fix issue with sorting when nested resource is present

--- packages/jsonapi-server/test/test-server.ts
+++ packages/jsonapi-server/test/test-server.ts
@@ -1,5 +1,5 @@
 import { Schema, createMemoryStore } from "data-prism";
-import careBearSchema from "./fixtures/care-bears.schema.json" assert { type: "json" };
+import careBearSchema from "./fixtures/care-bears.schema.json" with { type: "json" };
 import { careBearData } from "./fixtures/care-bear-data.js"; // eslint-disable-line
 import { createServer } from "../src/server.js";
 
