e7cbe101e9b8a76fbef59fc3546b3504c0675f32
Jake Sower <jsower@redzone.co>
2025-09-09 15:09:21 -0700

    start on qh

--- packages/query-helpers/src/helpers.js
+++ packages/query-helpers/src/helpers.js
@@ -1,15 +1,54 @@
 import { mapValues } from "es-toolkit";
 
-export function compileFormatter(templates) {
+export function compileFormatter(templates, pivot, keys) {
 	const fns = mapValues(
 		templates,
-		(template) =>
-			({ attribute, value }) =>
-				template
-					.replace(/\${attribute}/g, attribute)
-					.replace(/\${value}/g, value),
+		(template) => (vars) =>
+			keys.reduce((acc, k) => acc.replaceAll(`$\{${k}}`, vars[k]), template),
 	);
+	return (vars) => {
+		console.log({
+			templates,
+			pivot,
+			keys,
+			vars,
+			p: vars[pivot],
+			f: fns[vars[pivot]],
+			// x: fns[vars[pivot]](vars),
+		});
 
-	return ({ attribute, expressionName, value }) =>
-		fns[expressionName]({ attribute, value });
+		return fns[vars[pivot]](vars);
+	};
+}
+
+export function compileWhereFormatter(templates) {
+	return compileFormatter(templates, "expressionName", ["attribute", "value"]);
+}
+
+export function compileOrderFormatter(templates) {
+	return compileFormatter(templates, "direction", ["attribute"]);
+}
+
+export function compileResourceMappers(schema, type, mappers) {
+	const resSchema = schema.resources[type];
+	const mappable = { ...resSchema.attributes, ...resSchema.relationships };
+	const fns = mapValues(mappable, (_, name) => {
+		if (name in mappers) {
+			if (typeof mappers[name] === "function") {
+				return mappers[name];
+			} else if (typeof mappers[name] === "string") {
+				return (val) => val[mappers[name]];
+			} else {
+				throw new Error("mappers must be functions or strings");
+			}
+		}
+
+		return (val) => val[name];
+	});
+
+	return (resource) =>
+		Object.entries(fns).reduce((acc, [key, fn]) => {
+			const val = fn(resource);
+			return val === undefined ? acc : { ...acc, [key]: val };
+		}, {});
 }
